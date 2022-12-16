82b710ef738f93248f24d1fab5920b3e6abb7c9a
bkhoo123 <102838003+bkhoo123@users.noreply.github.com>
2022-12-16 21:44:37 +0000

    session

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -32,6 +32,24 @@ router.post(
 
 
 
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({ user: null });
+    }
+  );
+  
+  // ...
+
+
+
 // Log out
 router.delete(
     '/',
