13c593db7c5f4e23aa0914035d09696983f333bb
bkhoo123 <102838003+bkhoo123@users.noreply.github.com>
2022-12-16 21:26:29 +0000

    Add User authentication Express middleware

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,10 +1,72 @@
 // backend/routes/api/index.js
-const router = require('express').Router();
+const router = require("express").Router();
+const { restoreUser } = require("../../utils/auth.js");
 
-// was not an async function before
-router.post('/test', async function(req, res) {
-    res.json({ requestBody: req.body });
-});
-  
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+router.use(restoreUser);
 
 module.exports = router;
+
+
+// backend/routes/api/index.js
+// ...
+
+// GET /api/restore-user
+// const { restoreUser } = require('../../utils/auth.js');
+
+// router.use(restoreUser);
+
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// // backend/routes/api/index.js
+// // ...
+
+// router.use(restoreUser);
+
+// ...
+
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// ...
+
+// ...
+
+// was not an async function before
+// router.post('/test', async function(req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+
+// backend/routes/api/index.js
+// ...
+
+// GET /api/set-token-cookie
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+
+// ...
+
